7d67f49397a1344639bdc23db0a62042431646f1
1 <eiby@163.com>
2015-08-06 15:34:51 +0800

    Rejected commit: Default

--- db.js
+++ db.js
@@ -1,27 +1,6 @@
 //connect to DB
 var mongoose = require('mongoose');
-//var db = mongoose.connect('mongodb://42.121.109.221:20099/baba');
 var db = mongoose.connect('mongodb://182.254.214.210:20099/baba, mongodb://182.254.215.229:20099/baba, mongodb://182.254.215.35:20099/baba', {read_secondary:true}); //业务服务器
-
-Date.prototype.format = function(format)
-{
-    var o = {
-        "M+" : this.getMonth()+1, //month
-        "d+" : this.getDate(),    //day
-        "h+" : this.getHours(),   //hour
-        "m+" : this.getMinutes(), //minute
-        "s+" : this.getSeconds(), //second
-        "q+" : Math.floor((this.getMonth()+3)/3),  //quarter
-        "S" : this.getMilliseconds() //millisecond
-    };
-    if(/(y+)/.test(format)) format=format.replace(RegExp.$1,
-        (this.getFullYear()+"").substr(4 - RegExp.$1.length));
-    for(var k in o)if(new RegExp("("+ k +")").test(format))
-        format = format.replace(RegExp.$1,
-            RegExp.$1.length==1 ? o[k] :
-                ("00"+ o[k]).substr((""+ o[k]).length));
-    return format;
-};
 
 // Define Model
 var Schema = mongoose.Schema;
